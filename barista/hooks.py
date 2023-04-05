c79fa1777da26445f4d930c3072c8578aa3e2c03
poojavadher24 <poojavadher24@gmail.com>
2023-04-05 18:22:55 +0530

    Added Public workspace and notify to doctype

--- barista/hooks.py
+++ barista/hooks.py
@@ -120,23 +120,9 @@ app_license = "MIT"
 # 	"frappe.desk.doctype.event.event.get_events": "barista.event.get_events"
 # }
 fixtures = [
-    "Test Data",
-    "Test Case",
-    "Test Suite",
-    # {"dt": "Workspace", "filters": [
-    # [
-    #     "name", "in", [
-    #     "Barista-Administrator"
-    # ]
-    # ]
-    # ]},
-    # {"dt": "Custom Field", "filters": [
-    # [
-    #     "dt", "in", [
-    #     "Test Suite"
-    # ]
-    # ]
-    # ]},
+    # "Test Data",
+    # "Test Case",
+    # "Test Suite",
     # "Testcase Item",
     # "Testdata Item",
     # "Assertion",
